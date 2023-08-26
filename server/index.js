87ca195f9b9eccb8dc704b02e36640c8a4a0f4e9
Maaz-Ur-Rehman <maazurrehman42@gmail.com>
2023-08-26 22:47:19 +0500

    build added

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from "express";
-import dotenv, { config } from "dotenv";
+import dotenv from "dotenv"; // Removed unused { config }
 import cors from "cors";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import connection from "./database/db.js";
 import Routes from "./routes/routes.js";
-import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 const app = express();
 dotenv.config();
 app.use(bodyParser.json({ extended: true }));
@@ -17,13 +19,19 @@ const userpassword = process.env.DB_password;
 connection(username, userpassword);
 app.use(cookieParser())
 app.use(cors());
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-
-app.use(express.static(path.join(__dirname, './client/build')));
 app.use("/", Routes);
 
-app.use("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"./client/build/index.html"))
-})
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+import path from "path"; // Import the path module
+
+app.use(express.static(path.join(__dirname, "../client/build")));
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../client/build/index.html"),()=>{
+        res.status(500).send(err)
+    })
+});
+
 const port = 7000;
 app.listen(port, () => console.log(`server is running on ${port} port`));
